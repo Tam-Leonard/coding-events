9dbe78f31b7c6fc8a7139a5b3710970049cab2cd
Tamara Leonard <LeonardTamaraD@gmail.com>
2023-08-01 15:03:10 -0500

    ch 14 examples complete

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,11 +1,10 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,30 +13,55 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-   private static List<String> events = new ArrayList<>();
+   //private static List<Event> events = new ArrayList<>(); removed b/c created the eventdata class
    //this was made b/c the list below is
     //private and can't be accessed outside of the displayAllEvents method so createEvent method can
     //access it or take data that is passed in
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("events", events);
+        model.addAttribute("title", "All Events");
+        model.addAttribute("events", EventData.getAll());
+        //updated from "events",event b/c got rid of the arraylist above b/c we created eventdata class
         return "events/index";
     }
 
     //This method lives at /events/create
    @GetMapping("create")
-    public String renderCreateEvent(){
-        return "events/create";
+   //eventName parameter comes from create.html
+   public String displayCreateEventForm(Model model) {
+       model.addAttribute("title","Create Event");
+       return "events/create";
     }
-
     //lives at /event/create
     @PostMapping("create")
-    //eventName parameter comes from create.html
-    public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+    public String processCreateEventForm(@ModelAttribute Event newEvent) {
+        EventData.add(newEvent);
+        return "redirect:";
+        //when the Event newEvent is called, Spring will create the new event for us
+        //updated from on create.html file from eventName to just name, eventDescription to description
+
+    /*This commented out for model binding public String processCreateEventForm(@RequestParam String eventName,
+                                         @RequestParam String eventDescription) {
+        EventData.add(new Event(eventName, eventDescription));
+        //this was updated from events eventdata b/c we removed the arraylist and created eventdata class
         return "redirect:";
         //redirect response that instructs the browser to go to a different page
-        //the colon instructs user to redirect to a specific controller, the root path-/events
+        //the colon instructs user to redirect to a specific controller, the root path-/events*/
+    }
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
 
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+        return "redirect:";
     }
 }
